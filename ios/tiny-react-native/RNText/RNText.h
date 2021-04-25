5d035954d08ae74eb10b55140bb52a0bc8b3f79e
youxingzhi <xingzhi.you@shopee.com>
2021-04-25 11:35:18 +0800

    don't use create-react-app

---
+++ ios/tiny-react-native/RNText/RNText.h
@@ -0,0 +1,17 @@
+//
+//  RNText.h
+//  tiny-react-native
+//
+//  Created by You Xingzhi on 2021/4/25.
+//  Copyright © 2021 youxingzhi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNText : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/tiny-react-native/RNText/RNText.m
@@ -0,0 +1,13 @@
+//
+//  RNText.m
+//  tiny-react-native
+//
+//  Created by You Xingzhi on 2021/4/25.
+//  Copyright © 2021 youxingzhi. All rights reserved.
+//
+
+#import "RNText.h"
+
+@implementation RNText
+
+@end
